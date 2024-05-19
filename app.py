fa5a9cc6848e890881d0ab3d323757c98ce9d1fb
LavanyaY2 <yadavlavanya475@gmail.com>
2024-05-19 13:06:49 -0400

    some ui changes

--- app.py
+++ app.py
@@ -2,7 +2,16 @@ import audio_to_txt
 import streamlit as st
 
 from pydub import AudioSegment
+import pandas as pd
+from io import StringIO, BytesIO
 
-audio = AudioSegment.from_wav('test_audio.wav')
-response = audio_to_txt.convert_audio(audio)
-st.write(response)
+st.button("Upload a file")
+uploaded_file = st.file_uploader("Choose a file")
+    
+if uploaded_file is not None:
+    bytes_data = uploaded_file.getvalue()
+    st.write("File Uploaded Succesfully")
+    audio = AudioSegment.from_file(BytesIO(bytes_data), format = "wav")
+    response = audio_to_txt.convert_audio(audio)
+    st.write(response)
+# st.button("Upload a file", type="primary")
